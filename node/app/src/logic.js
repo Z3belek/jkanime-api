0f06a2b0016f82de75263ee6f1f5ff242b3209a2
Sebastián Ibañez <77544506+Z3belek@users.noreply.github.com>
2024-04-30 12:34:06 -0300

    Add library of all anime route and logic

--- node/app/src/logic.js
+++ node/app/src/logic.js
@@ -32,3 +32,41 @@ export const getLatestAnimes = async (req, res) => {
     res.status(500).json({ error: 'Hubo un error al obtener los animes más recientes' });
   }
 };
+
+export const getAllAnimes = async (req, res) => {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+  try {
+    const pageNumber = req.query.page || 1;
+    const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+
+    await page.goto(`https://jkanime.net/directorio/${pageNumber}`);
+
+    const { data: animeList, morePages } = await page.evaluate(() => {
+      const animeItems = Array.from(document.querySelectorAll(".page_directorio .custom_item2"));
+      const nextPage = document.querySelector("div.navigation a.nav-next");
+
+      const data = animeItems.map(anime => ({
+        title: anime.querySelector("h5 a").textContent,
+        id: anime.querySelector("a").href.split("/")[3],
+        poster: anime.querySelector("img").src,
+        status: anime.querySelector("div.card-info p.card-status").textContent.trim(),
+        animeType: anime.querySelector("div.card-info p.card-txt").textContent.trim(),
+        synopsis: anime.querySelector("div.card-body p.synopsis").textContent.trim()
+      }));
+
+      return { data, morePages: nextPage ? true : false };
+    });
+
+    await browser.close();
+
+    res.json({ animeList, morePages });
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Hubo un error al obtener los animes' });
+  }
+};
--- node/app/src/routes.js
+++ node/app/src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getLatestAnimes } from "./logic.js";
+import { getLatestAnimes, getAllAnimes } from "./logic.js";
 
 const logicRoutes = Router();
 
 logicRoutes.get("/latest-animes", getLatestAnimes);
+
+logicRoutes.get("/all-animes", getAllAnimes);
 
 export default logicRoutes;
